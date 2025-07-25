389e34be8e90f0b518242585f1795075663e711a
cktsang <ck.tsang@hotmail.com>
2025-07-26 00:18:20 +0200

    Removed nodemon, using node --watch mode instead

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,12 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 app.use(express.json());
-
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log("server is running on port 5001");
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("server is running on port 5001");
+  });
 });
