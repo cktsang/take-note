c7ecb54877f85f0fff56f2b308f5376330dd0ad3
cktsang <ck.tsang@hotmail.com>
2025-08-07 14:05:17 +0200

    Fixed cors error

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(express.json());
 app.use("/api/notes", notesRoutes);
